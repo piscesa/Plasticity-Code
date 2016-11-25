374a43faf955992b9136cfcc5d1e8821e3ce893d
Bambang Piscesa <bambang.piscesa@live.com>
2016-11-25 12:30:25 +1100

    Add files via upload

---
+++ Eccentricity/Eccentricity/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Eccentricity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input Data
+            double fbc, eccnew, alphanew, kesi, m, ft, fr, k1, fcc, rho, J2, r, rfbc;
+            double a, b, c;
+            double fc = 120;
+            double ecc = 0.52;
+            double alpha = 1;
+            int SF = 0;
+            const double tol9 = 0.000000001;
+
+            // Calculate basic parameter
+            fbc = 1.5 * Pow(fc, 0.925);
+            rfbc = fbc /fc;
+            kesi = -2.0 * fbc / Sqrt(3.0);
+            J2 = 2.0 / 6.0 * Pow(-fbc, 2.0);
+            rho = Sqrt(2.0 * J2);
+            if (SF == 0)
+            {
+                ft = 0.9 * 0.32 * Pow(fc, 0.67);
+            }
+            else
+            {
+                ft = 0.9 * 0.62 * Pow(fc, 0.5);
+            }
+            first:;
+            m = 3.0 * (fc * fc - ft * ft) / (fc * ft);
+            m *= ecc / (ecc + 1.0);
+
+            // Calculate fr
+            a = 9.0;
+            b = -(alpha * m * fc - 6.0 * Sqrt(3.0) * kesi);
+            c = 3.0 * kesi * kesi - fc * fc + 2.0 * Sqrt(3.0) * kesi * m * (1.0 - alpha) * fc / 6.0;
+            fr = -b - Sqrt(b * b - 4.0 * a * c);
+            fr /= 2.0 * a;
+
+            // Calculate fcc (Attard and Setunge - 1996)
+            k1 = 1.25 * (1.0 + 0.062 * fr / fc) * Pow(fc, -0.21);
+            fcc = fc * Pow(fr / ft + 1.0, k1);
+
+            // Calculate AlphaNew
+            alphanew = 1.0 - ((fc * fc + m * fr * fc - Pow(fcc - fr, 2.0)) / (m * fc / 3.0 * (fr - fcc)));
+
+            if (Abs(alpha - alphanew) > tol9)
+            {
+                alpha = alphanew;
+                goto first;
+            }
+
+            r = 1.0 - 1.5 * rho * rho / (fc * fc) - m * kesi / (Sqrt(3.0) * fc);
+            r *= Sqrt(6.0) * fc / (rho * m * alphanew);
+            eccnew = 1.0 / r;
+
+            if (Abs(ecc - eccnew) > tol9)
+            {
+                ecc = eccnew;
+                goto first;
+            }
+
+            Console.WriteLine("fc = " + fc + "MPa ecc = " + eccnew);
+            Console.WriteLine("I1A = " + (fcc + 2.0 * fr) + "MPa I1B = " + (2.0 * fbc) + " MPa");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Eccentricity/Eccentricity/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Eccentricity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Eccentricity")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c67103b-408d-4fa0-92cf-3265a3d33a49")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
